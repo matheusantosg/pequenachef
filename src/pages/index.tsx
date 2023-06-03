4534ceafc27720b228a0785b27f375c1a04f1676
matheusantosg <matheusantosgdeveloper@gmail.com>
2023-06-03 03:36:20 -0300

    ajustes do CMS e puxando dados da API Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Header } from '../components/Header'
 import { BannerTop } from '../components/BannerTop'
 import { AboutUs } from '../components/AboutUs'
 import { MenuChef } from '../components/MenuChef'
+import { createClient } from '../../prismicio';
+import { RichText } from 'prismic-dom';
 
 export default function Home() {
   return (
@@ -18,4 +20,19 @@ export default function Home() {
       <MenuChef />
     </>
   )
+}
+
+export async function getStaticProps({ previewData }) {
+  const client = createClient({ previewData });
+
+  const response = await client.getSingle('homepage');
+  
+  const tipbar = response.data.tipbar.map((tipbar) => ({
+    text: tipbar.text_tipbar[0].text
+  }))
+
+  console.log(tipbar)
+  return {
+    props: { tipbar },
+  };
 }
---
+++ src/services/prismic.ts
@@ -0,0 +1,10 @@
+import Prismic from '@prismicio/client'
+
+export function getPrismicClient() {
+  const prismic = new Prismic.Client(process.env.PRISMIC_ENDPOINT,
+    {
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN
+    })
+
+  return prismic
+}
